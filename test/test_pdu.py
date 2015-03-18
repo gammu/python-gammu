f39a9898cc513838811cacddfb895bf9e554aee2
Michal Čihař <michal@cihar.com>
2015-03-18 10:08:53 +0100

    Implement another PDU test
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- test/test_pdu.py
+++ test/test_pdu.py
@@ -4,6 +4,13 @@ import gammu
 import sys
 
 PDU_DATA = '079124602009999002AB098106845688F8907080517375809070805183018000'
+MESSAGE = (
+    '.........1.........2.........3.........4.........5.........6.........7'
+    '.........8.........9........0.........1.........2.........3.........4'
+    '.........5.........6.........7.........8.........9........0.........1'
+    '.........2.........3.........4.........5.........6.........7.........8'
+    '.........9........0'
+)
 
 
 class PDUTest(unittest.TestCase):
@@ -15,3 +22,31 @@ class PDUTest(unittest.TestCase):
         sms = gammu.DecodePDU(PDU_DATA.decode('hex'))
         self.assertEqual(sms['Number'], '604865888')
         self.assertEqual(sms['Text'], 'Delivered')
+
+    def test_smsinfo(self):
+        # text of sms
+        # SMS info about message
+        smsinfo = {
+            'Entries': [{'ID': 'ConcatenatedTextLong', 'Buffer': MESSAGE}]
+        }
+
+        # encode SMSes
+        sms = gammu.EncodeSMS(smsinfo)
+
+        # decode back SMSes
+        decodedsms = gammu.DecodeSMS(sms)
+
+        # compare text
+        self.assertEqual(decodedsms['Entries'][0]['Buffer'], MESSAGE)
+
+        # do conversion to PDU
+        pdu = [gammu.EncodePDU(s) for s in sms]
+
+        # Convert back
+        pdusms = [gammu.DecodePDU(p) for p in pdu]
+
+        # decode back SMS from PDU
+        decodedsms = gammu.DecodeSMS(pdusms)
+
+        # compare PDU results
+        self.assertEqual(decodedsms['Entries'][0]['Buffer'], MESSAGE)

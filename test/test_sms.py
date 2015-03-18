4a6f11ef9e28f0327be4ad2144bc9b1478fb7663
Michal Čihař <michal@cihar.com>
2015-03-18 10:23:40 +0100

    Make debug log optional
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- test/test_sms.py
+++ test/test_sms.py
@@ -23,6 +23,7 @@ import unittest
 import doctest
 import gammu
 import sys
+import os
 
 
 PDU_DATA = '079124602009999002AB098106845688F8907080517375809070805183018000'
@@ -51,8 +52,9 @@ GSM = (
 
 class PDUTest(unittest.TestCase):
     def setUp(self):
-        gammu.SetDebugFile(sys.stderr)
-        gammu.SetDebugLevel('textall')
+        if 'GAMMU_DEBUG' in os.environ:
+            gammu.SetDebugFile(sys.stderr)
+            gammu.SetDebugLevel('textall')
 
     def test_decode(self):
         sms = gammu.DecodePDU(PDU_DATA.decode('hex'))

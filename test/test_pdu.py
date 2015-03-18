916b2ca587e2d0f85000f33e6549e7f184860ca8
Michal Čihař <michal@cihar.com>
2015-03-18 10:11:41 +0100

    Convert another testcase
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- test/test_pdu.py
+++ test/test_pdu.py
@@ -1,7 +1,28 @@
+# -*- coding: UTF-8 -*-
+# vim: expandtab sw=4 ts=4 sts=4:
+#
+# Copyright © 2003 - 2015 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Gammu <http://wammu.eu/>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+from __future__ import unicode_literals
 import unittest
 import doctest
 import gammu
 import sys
+
 
 PDU_DATA = '079124602009999002AB098106845688F8907080517375809070805183018000'
 MESSAGE = (
@@ -10,6 +31,13 @@ MESSAGE = (
     '.........5.........6.........7.........8.........9........0.........1'
     '.........2.........3.........4.........5.........6.........7.........8'
     '.........9........0'
+)
+UNICODE = (
+    '.........1ě........2..ř......3...žš....4....ý....5....á....6....á....7'
+    '.........8.........9........0.........1.........2.........3.........4'
+    '.........5.........6.........7.........8.........9........0.........1'
+    '.........2.........3.........4.........5.........6.........7.........8'
+    '.........9....č...0'
 )
 
 
@@ -50,3 +78,32 @@ class PDUTest(unittest.TestCase):
 
         # compare PDU results
         self.assertEqual(decodedsms['Entries'][0]['Buffer'], MESSAGE)
+
+    def test_unicode(self):
+        # text of sms
+        # SMS info about message
+        smsinfo = {
+            'Entries': [{'ID': 'ConcatenatedTextLong', 'Buffer': UNICODE}],
+            'Unicode': True
+        }
+
+        # encode SMSes
+        sms = gammu.EncodeSMS(smsinfo)
+
+        # decode back SMSes
+        decodedsms = gammu.DecodeSMS(sms)
+
+        # compare text
+        self.assertEqual(decodedsms['Entries'][0]['Buffer'], UNICODE)
+
+        # do conversion to PDU
+        pdu = [gammu.EncodePDU(s) for s in sms]
+
+        # Convert back
+        pdusms = [gammu.DecodePDU(p) for p in pdu]
+
+        # decode back SMS from PDU
+        decodedsms = gammu.DecodeSMS(pdusms)
+
+        # compare PDU results
+        self.assertEqual(decodedsms['Entries'][0]['Buffer'], UNICODE)

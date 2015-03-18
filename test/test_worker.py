bfbadedf6ab0c7015e16e66b6a596e2fb0add660
Michal Čihař <michal@cihar.com>
2015-03-18 14:30:39 +0100

    Remove not needed import
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- test/test_worker.py
+++ test/test_worker.py
@@ -17,7 +17,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-import unittest
 import gammu.worker
 from test_dummy import DummyTest
 

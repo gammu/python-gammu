bad08b82af3739b6a3a5bb8e488e24e0ac571796
Michal Čihař <michal@cihar.com>
2015-03-20 11:50:13 +0100

    Move code to functions
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- examples/getallmemory.py
+++ examples/getallmemory.py
@@ -4,33 +4,39 @@ from __future__ import print_function
 import gammu
 import sys
 
-state_machine = gammu.StateMachine()
-state_machine.ReadConfig()
-state_machine.Init()
 
-if len(sys.argv) != 2:
-    print('This requires one parameter with memory type!')
-    sys.exit(1)
+def main():
+    state_machine = gammu.StateMachine()
+    state_machine.ReadConfig()
+    state_machine.Init()
 
-memory_type = sys.argv[1]
+    if len(sys.argv) != 2:
+        print('This requires one parameter with memory type!')
+        sys.exit(1)
 
-status = state_machine.GetMemoryStatus(Type=memory_type)
+    memory_type = sys.argv[1]
 
-remain = status['Used']
+    status = state_machine.GetMemoryStatus(Type=memory_type)
 
-start = True
+    remain = status['Used']
 
-while remain > 0:
-    if start:
-        entry = state_machine.GetNextMemory(Start=True, Type=memory_type)
-        start = False
-    else:
-        entry = state_machine.GetNextMemory(
-            Location=entry['Location'], Type=memory_type
-        )
-    remain = remain - 1
+    start = True
 
-    print()
-    print('%-15s: %d' % ('Location', entry['Location']))
-    for v in entry['Entries']:
-        print('%-15s: %s' % (v['Type'], str(v['Value'])))
+    while remain > 0:
+        if start:
+            entry = state_machine.GetNextMemory(Start=True, Type=memory_type)
+            start = False
+        else:
+            entry = state_machine.GetNextMemory(
+                Location=entry['Location'], Type=memory_type
+            )
+        remain = remain - 1
+
+        print()
+        print('%-15s: %d' % ('Location', entry['Location']))
+        for v in entry['Entries']:
+            print('%-15s: %s' % (v['Type'], str(v['Value'])))
+
+
+if __name__ == '__main__':
+    main()
--- examples/getallmemory_nonext.py
+++ examples/getallmemory_nonext.py
@@ -4,30 +4,36 @@ from __future__ import print_function
 import gammu
 import sys
 
-state_machine = gammu.StateMachine()
-state_machine.ReadConfig()
-state_machine.Init()
 
-if len(sys.argv) != 2:
-    print('This requires one parameter with memory type!')
-    sys.exit(1)
+def main():
+    state_machine = gammu.StateMachine()
+    state_machine.ReadConfig()
+    state_machine.Init()
 
-memory_type = sys.argv[1]
+    if len(sys.argv) != 2:
+        print('This requires one parameter with memory type!')
+        sys.exit(1)
 
-status = state_machine.GetMemoryStatus(Type=memory_type)
+    memory_type = sys.argv[1]
 
-remain = status['Used']
+    status = state_machine.GetMemoryStatus(Type=memory_type)
 
-location = 1
+    remain = status['Used']
 
-while remain > 0:
-    try:
-        entry = state_machine.GetMemory(Type=memory_type, Location=location)
-        print()
-        print('%-15s: %d' % ('Location', entry['Location']))
-        for v in entry['Entries']:
-            print('%-15s: %s' % (v['Type'], str(v['Value'])))
-        remain = remain - 1
-    except gammu.ERR_EMPTY:
-        pass
-    location = location + 1
+    location = 1
+
+    while remain > 0:
+        try:
+            entry = state_machine.GetMemory(Type=memory_type, Location=location)
+            print()
+            print('%-15s: %d' % ('Location', entry['Location']))
+            for v in entry['Entries']:
+                print('%-15s: %s' % (v['Type'], str(v['Value'])))
+            remain = remain - 1
+        except gammu.ERR_EMPTY:
+            pass
+        location = location + 1
+
+
+if __name__ == '__main__':
+    main()

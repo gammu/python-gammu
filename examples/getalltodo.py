863c658e58a06ef95e63bca8a5298bea593daa5e
Michal Čihař <michal@cihar.com>
2015-03-20 11:54:07 +0100

    Move code to functions
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- examples/getalltodo.py
+++ examples/getalltodo.py
@@ -3,26 +3,32 @@
 from __future__ import print_function
 import gammu
 
-state_machine = gammu.StateMachine()
-state_machine.ReadConfig()
-state_machine.Init()
 
-status = state_machine.GetToDoStatus()
+def main():
+    state_machine = gammu.StateMachine()
+    state_machine.ReadConfig()
+    state_machine.Init()
 
-remain = status['Used']
+    status = state_machine.GetToDoStatus()
 
-start = True
+    remain = status['Used']
 
-while remain > 0:
-    if start:
-        entry = state_machine.GetNextToDo(Start=True)
-        start = False
-    else:
-        entry = state_machine.GetNextToDo(Location=entry['Location'])
-    remain = remain - 1
+    start = True
 
-    print()
-    print('%-15s: %d' % ('Location', entry['Location']))
-    print('%-15s: %s' % ('Priority', entry['Priority']))
-    for v in entry['Entries']:
-        print('%-15s: %s' % (v['Type'], str(v['Value'])))
+    while remain > 0:
+        if start:
+            entry = state_machine.GetNextToDo(Start=True)
+            start = False
+        else:
+            entry = state_machine.GetNextToDo(Location=entry['Location'])
+        remain = remain - 1
+
+        print()
+        print('%-15s: %d' % ('Location', entry['Location']))
+        print('%-15s: %s' % ('Priority', entry['Priority']))
+        for v in entry['Entries']:
+            print('%-15s: %s' % (v['Type'], str(v['Value'])))
+
+
+if __name__ == '__main__':
+    main()

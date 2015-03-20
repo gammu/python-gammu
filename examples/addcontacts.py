c0dcdd817bebb1cd490e5caf81543fef1d65caf7
Michal Čihař <michal@cihar.com>
2015-03-20 09:19:17 +0100

    Move code to function
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- examples/addcontacts.py
+++ examples/addcontacts.py
@@ -4,23 +4,29 @@ from __future__ import print_function
 import gammu
 import sys
 
-if len(sys.argv) != 3:
-    print(
-        'This requires two parameters: '
-        'memory_type and backup file (eg. vcard)!'
-    )
-    sys.exit(1)
 
-state_machine = gammu.StateMachine()
-state_machine.ReadConfig()
-state_machine.Init()
+def main():
+    if len(sys.argv) != 3:
+        print(
+            'This requires two parameters: '
+            'memory_type and backup file (eg. vcard)!'
+        )
+        sys.exit(1)
 
-memory = sys.argv[1]
-filename = sys.argv[2]
+    state_machine = gammu.StateMachine()
+    state_machine.ReadConfig()
+    state_machine.Init()
 
-backup = gammu.ReadBackup(filename)
+    memory = sys.argv[1]
+    filename = sys.argv[2]
 
-for item in backup['PhonePhonebook']:
-    item['MemoryType'] = memory
-    loc = state_machine.AddMemory(item)
-    print(('Added item to location %d' % loc))
+    backup = gammu.ReadBackup(filename)
+
+    for item in backup['PhonePhonebook']:
+        item['MemoryType'] = memory
+        loc = state_machine.AddMemory(item)
+        print(('Added item to location %d' % loc))
+
+
+if __name__ == '__main__':
+    main()

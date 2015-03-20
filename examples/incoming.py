4352969f857f0ae552a184a26f50eac24bd47cff
Michal Čihař <michal@cihar.com>
2015-03-20 09:16:47 +0100

    Move code to function
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- examples/incoming.py
+++ examples/incoming.py
@@ -20,45 +20,49 @@ def Callback(state_machine, callback_type, data):
     print(data)
 
 
-# Create state machine
-state_machine = gammu.StateMachine()
-# Read gammurc
-state_machine.ReadConfig()
-# Initialize state machine and connect to phone
-state_machine.Init()
-# Set callback handler for incoming notifications
-state_machine.SetIncomingCallback(Callback)
+def main():
+    # Create state machine
+    state_machine = gammu.StateMachine()
+    # Read gammurc
+    state_machine.ReadConfig()
+    # Initialize state machine and connect to phone
+    state_machine.Init()
+    # Set callback handler for incoming notifications
+    state_machine.SetIncomingCallback(Callback)
 
-# Enable notifications from calls
-try:
-    state_machine.SetIncomingCall()
-except gammu.ERR_NOTSUPPORTED:
-    print('Incoming calls notification is not supported.')
+    # Enable notifications from calls
+    try:
+        state_machine.SetIncomingCall()
+    except gammu.ERR_NOTSUPPORTED:
+        print('Incoming calls notification is not supported.')
 
-# Enable notifications from cell broadcast
-try:
-    state_machine.SetIncomingCB()
-except gammu.ERR_NOTSUPPORTED:
-    print('Incoming CB notification is not supported.')
-except gammu.ERR_SOURCENOTAVAILABLE:
-    print('Cell broadcasts support not enabled in Gammu.')
+    # Enable notifications from cell broadcast
+    try:
+        state_machine.SetIncomingCB()
+    except gammu.ERR_NOTSUPPORTED:
+        print('Incoming CB notification is not supported.')
+    except gammu.ERR_SOURCENOTAVAILABLE:
+        print('Cell broadcasts support not enabled in Gammu.')
 
-# Enable notifications from incoming SMS
-try:
-    state_machine.SetIncomingSMS()
-except gammu.ERR_NOTSUPPORTED:
-    print('Incoming SMS notification is not supported.')
+    # Enable notifications from incoming SMS
+    try:
+        state_machine.SetIncomingSMS()
+    except gammu.ERR_NOTSUPPORTED:
+        print('Incoming SMS notification is not supported.')
 
-# Enable notifications for incoming USSD
-try:
-    state_machine.SetIncomingUSSD()
-except gammu.ERR_NOTSUPPORTED:
-    print('Incoming USSD notification is not supported.')
+    # Enable notifications for incoming USSD
+    try:
+        state_machine.SetIncomingUSSD()
+    except gammu.ERR_NOTSUPPORTED:
+        print('Incoming USSD notification is not supported.')
 
-# Just a busy waiting for event
-# We need to keep communication with phone to get notifications
-print('Press Ctrl+C to interrupt')
-while 1:
-    q = state_machine.GetSignalQuality()
-    print('Signal is at %d%%' % q['SignalPercent'])
-    time.sleep(1)
+    # Just a busy waiting for event
+    # We need to keep communication with phone to get notifications
+    print('Press Ctrl+C to interrupt')
+    while 1:
+        q = state_machine.GetSignalQuality()
+        print('Signal is at %d%%' % q['SignalPercent'])
+        time.sleep(1)
+
+if __name__ == '__main__':
+    main()

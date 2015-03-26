e66b2acf1233bef18d62d197a2e8b583cbb6c82e
Michal Čihař <michal@cihar.com>
2015-03-26 16:45:39 +0100

    Move code to a function
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- examples/filesystem_test.py
+++ examples/filesystem_test.py
@@ -31,226 +31,232 @@ import datetime
 import sys
 from optparse import OptionParser
 
-parser = OptionParser(usage="usage: %prog [options]")
 
-parser.add_option("-c", "--config",
-                  action="store", type="string",
-                  dest="config", default=None,
-                  help="Config file path")
-parser.add_option("-f", "--folder",
-                  action="store", type="string",
-                  dest="folder", default=None,
-                  help="Folder to be used for testing")
-parser.add_option("-t", "--test-file",
-                  action="store", type="string",
-                  dest="testfile", default="./data/cgi.jpg",
-                  help="Local file to be used for testing")
-(options, args) = parser.parse_args()
+def main():
+    parser = OptionParser(usage="usage: %prog [options]")
 
-if options.folder is None:
-    print("You have to select folder where testing will be done!")
-    print("And even better, you should read the script before you run it.")
-    sys.exit(1)
+    parser.add_option("-c", "--config",
+                      action="store", type="string",
+                      dest="config", default=None,
+                      help="Config file path")
+    parser.add_option("-f", "--folder",
+                      action="store", type="string",
+                      dest="folder", default=None,
+                      help="Folder to be used for testing")
+    parser.add_option("-t", "--test-file",
+                      action="store", type="string",
+                      dest="testfile", default="./data/cgi.jpg",
+                      help="Local file to be used for testing")
+    (options, args) = parser.parse_args()
 
-if not os.path.exists(options.testfile):
-    print("You have to select file which will be used for testing!")
-    sys.exit(1)
+    if options.folder is None:
+        print("You have to select folder where testing will be done!")
+        print("And even better, you should read the script before you run it.")
+        sys.exit(1)
 
-state_machine = gammu.StateMachine()
-if options.config is not None:
-    state_machine.ReadConfig(Filename=options.config)
-else:
-    state_machine.ReadConfig()
-state_machine.Init()
+    if not os.path.exists(options.testfile):
+        print("You have to select file which will be used for testing!")
+        sys.exit(1)
 
-# Check GetFileSystemStatus
-print("Expection: Info about filesystem usage")
-try:
-    fs_info = state_machine.GetFileSystemStatus()
-    fs_info["Total"] = fs_info["Free"] + fs_info["Used"]
-    print("Used: %(Used)d, Free: %(Free)d, Total: %(Total)d" % fs_info)
-except gammu.ERR_NOTSUPPORTED:
-    print("You will have to live without this knowledge")
+    state_machine = gammu.StateMachine()
+    if options.config is not None:
+        state_machine.ReadConfig(Filename=options.config)
+    else:
+        state_machine.ReadConfig()
+    state_machine.Init()
 
-# Check DeleteFile
-print("\n\nExpection: Deleting cgi.jpg from memorycard")
-try:
-    state_machine.DeleteFile(unicode(options.folder + "/cgi.jpg"))
-except gammu.ERR_FILENOTEXIST:
-    print("Oh well - we copy it now ;-) (You SHOULD read this)")
+    # Check GetFileSystemStatus
+    print("Expection: Info about filesystem usage")
+    try:
+        fs_info = state_machine.GetFileSystemStatus()
+        fs_info["Total"] = fs_info["Free"] + fs_info["Used"]
+        print("Used: %(Used)d, Free: %(Free)d, Total: %(Total)d" % fs_info)
+    except gammu.ERR_NOTSUPPORTED:
+        print("You will have to live without this knowledge")
 
-# Check AddFilePart
-print("\n\nExpection: Put cgi.jpg onto Memorycard on phone")
-file_handle = open(options.testfile, "r")
-file_stat = os.stat(options.testfile)
-ttime = datetime.datetime.fromtimestamp(file_stat[8])
-file_f = {
-    "ID_FullName": options.folder,
-    "Name": u"cgi.jpg",
-    "Modified": ttime,
-    "Folder": 0,
-    "Level": 1,
-    "Used": file_stat[6],
-    "Buffer": file_handle.read(),
-    "Type": "Other",
-    "Protected": 0,
-    "ReadOnly": 0,
-    "Hidden": 0,
-    "System": 0,
-    "Handle": 0,
-    "Pos": 0,
-    "Finished": 0
-}
-while not file_f["Finished"]:
-    file_f = state_machine.AddFilePart(file_f)
+    # Check DeleteFile
+    print("\n\nExpection: Deleting cgi.jpg from memorycard")
+    try:
+        state_machine.DeleteFile(unicode(options.folder + "/cgi.jpg"))
+    except gammu.ERR_FILENOTEXIST:
+        print("Oh well - we copy it now ;-) (You SHOULD read this)")
 
-
-# Check GetFilePart
-print("\n\nExpection: Get cgi.jpg from memorycard and write it as test.jpg")
-with open('./test.jpg', 'w') as handle:
+    # Check AddFilePart
+    print("\n\nExpection: Put cgi.jpg onto Memorycard on phone")
+    file_handle = open(options.testfile, "r")
+    file_stat = os.stat(options.testfile)
+    ttime = datetime.datetime.fromtimestamp(file_stat[8])
     file_f = {
-        "ID_FullName": options.folder + "/cgi.jpg",
+        "ID_FullName": options.folder,
+        "Name": u"cgi.jpg",
+        "Modified": ttime,
+        "Folder": 0,
+        "Level": 1,
+        "Used": file_stat[6],
+        "Buffer": file_handle.read(),
+        "Type": "Other",
+        "Protected": 0,
+        "ReadOnly": 0,
+        "Hidden": 0,
+        "System": 0,
+        "Handle": 0,
+        "Pos": 0,
         "Finished": 0
     }
     while not file_f["Finished"]:
-        file_f = state_machine.GetFilePart(file_f)
-    handle.write(file_f["Buffer"])
-    handle.flush()
+        file_f = state_machine.AddFilePart(file_f)
 
-# Check correct transfer
-print("\n\nExpection: test.jpg and cgi.jpg to be the same")
-f1 = open(options.testfile, "r")
-f2 = open("./test.jpg", "r")
-if(f1.read() == f2.read()):
-    print("Same files")
-else:
-    print("Files differ!")
 
-os.remove("./test.jpg")
+    # Check GetFilePart
+    print("\n\nExpection: Get cgi.jpg from memorycard and write it as test.jpg")
+    with open('./test.jpg', 'w') as handle:
+        file_f = {
+            "ID_FullName": options.folder + "/cgi.jpg",
+            "Finished": 0
+        }
+        while not file_f["Finished"]:
+            file_f = state_machine.GetFilePart(file_f)
+        handle.write(file_f["Buffer"])
+        handle.flush()
 
-# Check GetNextRootFolder
-print("\n\nExpection: Root Folder List")
-try:
-    file_obj = state_machine.GetNextRootFolder(u"")
+    # Check correct transfer
+    print("\n\nExpection: test.jpg and cgi.jpg to be the same")
+    f1 = open(options.testfile, "r")
+    f2 = open("./test.jpg", "r")
+    if(f1.read() == f2.read()):
+        print("Same files")
+    else:
+        print("Files differ!")
+
+    os.remove("./test.jpg")
+
+    # Check GetNextRootFolder
+    print("\n\nExpection: Root Folder List")
+    try:
+        file_obj = state_machine.GetNextRootFolder(u"")
+        while 1:
+            print(file_obj["ID_FullName"] + " - " + file_obj["Name"])
+            try:
+                file_obj = state_machine.GetNextRootFolder(file_obj["ID_FullName"])
+            except gammu.ERR_EMPTY:
+                break
+    except gammu.ERR_NOTSUPPORTED:
+        print("Not supported...")
+
+
+    # Check GetNextFileFolder
+    print("\n\nExpection: Info for a file of the phone (cgi.jpg)")
+    file_f = state_machine.GetNextFileFolder(1)
     while 1:
-        print(file_obj["ID_FullName"] + " - " + file_obj["Name"])
-        try:
-            file_obj = state_machine.GetNextRootFolder(file_obj["ID_FullName"])
-        except gammu.ERR_EMPTY:
+        if(file_f["Name"] != "cgi.jpg"):
+            file_f = state_machine.GetNextFileFolder(0)
+        else:
+            attribute = ""
+            if file_f["Protected"]:
+                attribute = attribute + "P"
+            if file_f["ReadOnly"]:
+                attribute = attribute + "R"
+            if file_f["Hidden"]:
+                attribute = attribute + "H"
+            if file_f["System"]:
+                attribute = attribute + "S"
+            print((
+                "ID:         " + file_f["ID_FullName"] + "\n" +
+                "Name:       " + file_f["Name"] + "\n" +
+                "Folder:     " + str(file_f["Folder"]) + "\n" +
+                "Used:       " + str(file_f["Used"]) + "\n" +
+                "Modified:   " + file_f["Modified"].strftime("%x %X") + "\n" +
+                "Type:       " + file_f["Type"] + "\n" +
+                "Level:      " + str(file_f["Level"]) + "\n" +
+                "Attribute:  " + attribute
+            ))
+
             break
-except gammu.ERR_NOTSUPPORTED:
-    print("Not supported...")
+
+    # Check SetFileAttributes
+    # Protected is spared, as my mobile nokia 6230i says it's unsupported
+    print(
+        "\n\nExpection: Modifying attributes "
+        "(readonly=1, protected=0, system=1, hidden=1)"
+    )
+    state_machine.SetFileAttributes(
+        unicode(options.folder + "/cgi.jpg"), 1, 0, 1, 1
+    )
+
+    # Check GetFolderListing
+    print("\n\nExpection: Listing of cgi.jpg's properties")
+    file_f = state_machine.GetFolderListing(unicode(options.folder), 1)
+    while 1:
+        if(file_f["Name"] != "cgi.jpg"):
+            file_f = state_machine.GetFolderListing(unicode(options.folder), 0)
+        else:
+            attribute = ""
+            if file_f["Protected"]:
+                attribute = attribute + "P"
+            if file_f["ReadOnly"]:
+                attribute = attribute + "R"
+            if file_f["Hidden"]:
+                attribute = attribute + "H"
+            if file_f["System"]:
+                attribute = attribute + "S"
+            print((
+                "ID:         " + file_f["ID_FullName"] + "\n" +
+                "Name:       " + file_f["Name"] + "\n" +
+                "Folder:     " + str(file_f["Folder"]) + "\n" +
+                "Used:       " + str(file_f["Used"]) + "\n" +
+                "Modified:   " + file_f["Modified"].strftime("%x %X") + "\n" +
+                "Type:       " + file_f["Type"] + "\n" +
+                "Level:      " + str(file_f["Level"]) + "\n" +
+                "Attribute:  " + attribute
+            ))
+
+            break
+
+    # Check DeleteFile
+    print("\n\nExpection: Deletion of cgi.jpg from memorycard")
+    try:
+        state_machine.DeleteFile(unicode(options.folder + "cgi.jpg"))
+        print("Deleted")
+    except gammu.ERR_FILENOTEXIST:
+        print("Something is wrong ...")
+
+    # Check AddFolder
+    print("\n\nExpection: Creation of a folder on the memorycard \"42alpha\"")
+    file_f = state_machine.AddFolder(unicode(options.folder), u"42alpha")
+
+    # Check GetFolderListing again *wired*
+    print("\n\nExpection: Print properties of newly created folder")
+    file_f = state_machine.GetFolderListing(unicode(options.folder), 1)
+    while 1:
+        if(file_f["Name"] != "42alpha"):
+            file_f = state_machine.GetFolderListing(unicode(options.folder), 0)
+        else:
+            attribute = ""
+            if file_f["Protected"]:
+                attribute = attribute + "P"
+            if file_f["ReadOnly"]:
+                attribute = attribute + "R"
+            if file_f["Hidden"]:
+                attribute = attribute + "H"
+            if file_f["System"]:
+                attribute = attribute + "S"
+            print((
+                "ID:         " + file_f["ID_FullName"] + "\n" +
+                "Name:       " + file_f["Name"] + "\n" +
+                "Folder:     " + str(file_f["Folder"]) + "\n" +
+                "Used:       " + str(file_f["Used"]) + "\n" +
+                "Modified:   " + file_f["Modified"].strftime("%x %X") + "\n" +
+                "Type:       " + file_f["Type"] + "\n" +
+                "Level:      " + str(file_f["Level"]) + "\n" +
+                "Attribute:  " + attribute
+            ))
+
+            break
+
+    # Check DeleteFolder
+    print("\n\nExpection: Deletion of previously created folder \"42alpha\"")
+    state_machine.DeleteFolder(unicode(options.folder + "/42alpha"))
 
 
-# Check GetNextFileFolder
-print("\n\nExpection: Info for a file of the phone (cgi.jpg)")
-file_f = state_machine.GetNextFileFolder(1)
-while 1:
-    if(file_f["Name"] != "cgi.jpg"):
-        file_f = state_machine.GetNextFileFolder(0)
-    else:
-        attribute = ""
-        if file_f["Protected"]:
-            attribute = attribute + "P"
-        if file_f["ReadOnly"]:
-            attribute = attribute + "R"
-        if file_f["Hidden"]:
-            attribute = attribute + "H"
-        if file_f["System"]:
-            attribute = attribute + "S"
-        print((
-            "ID:         " + file_f["ID_FullName"] + "\n" +
-            "Name:       " + file_f["Name"] + "\n" +
-            "Folder:     " + str(file_f["Folder"]) + "\n" +
-            "Used:       " + str(file_f["Used"]) + "\n" +
-            "Modified:   " + file_f["Modified"].strftime("%x %X") + "\n" +
-            "Type:       " + file_f["Type"] + "\n" +
-            "Level:      " + str(file_f["Level"]) + "\n" +
-            "Attribute:  " + attribute
-        ))
-
-        break
-
-# Check SetFileAttributes
-# Protected is spared, as my mobile nokia 6230i says it's unsupported
-print(
-    "\n\nExpection: Modifying attributes "
-    "(readonly=1, protected=0, system=1, hidden=1)"
-)
-state_machine.SetFileAttributes(
-    unicode(options.folder + "/cgi.jpg"), 1, 0, 1, 1
-)
-
-# Check GetFolderListing
-print("\n\nExpection: Listing of cgi.jpg's properties")
-file_f = state_machine.GetFolderListing(unicode(options.folder), 1)
-while 1:
-    if(file_f["Name"] != "cgi.jpg"):
-        file_f = state_machine.GetFolderListing(unicode(options.folder), 0)
-    else:
-        attribute = ""
-        if file_f["Protected"]:
-            attribute = attribute + "P"
-        if file_f["ReadOnly"]:
-            attribute = attribute + "R"
-        if file_f["Hidden"]:
-            attribute = attribute + "H"
-        if file_f["System"]:
-            attribute = attribute + "S"
-        print((
-            "ID:         " + file_f["ID_FullName"] + "\n" +
-            "Name:       " + file_f["Name"] + "\n" +
-            "Folder:     " + str(file_f["Folder"]) + "\n" +
-            "Used:       " + str(file_f["Used"]) + "\n" +
-            "Modified:   " + file_f["Modified"].strftime("%x %X") + "\n" +
-            "Type:       " + file_f["Type"] + "\n" +
-            "Level:      " + str(file_f["Level"]) + "\n" +
-            "Attribute:  " + attribute
-        ))
-
-        break
-
-# Check DeleteFile
-print("\n\nExpection: Deletion of cgi.jpg from memorycard")
-try:
-    state_machine.DeleteFile(unicode(options.folder + "cgi.jpg"))
-    print("Deleted")
-except gammu.ERR_FILENOTEXIST:
-    print("Something is wrong ...")
-
-# Check AddFolder
-print("\n\nExpection: Creation of a folder on the memorycard \"42alpha\"")
-file_f = state_machine.AddFolder(unicode(options.folder), u"42alpha")
-
-# Check GetFolderListing again *wired*
-print("\n\nExpection: Print properties of newly created folder")
-file_f = state_machine.GetFolderListing(unicode(options.folder), 1)
-while 1:
-    if(file_f["Name"] != "42alpha"):
-        file_f = state_machine.GetFolderListing(unicode(options.folder), 0)
-    else:
-        attribute = ""
-        if file_f["Protected"]:
-            attribute = attribute + "P"
-        if file_f["ReadOnly"]:
-            attribute = attribute + "R"
-        if file_f["Hidden"]:
-            attribute = attribute + "H"
-        if file_f["System"]:
-            attribute = attribute + "S"
-        print((
-            "ID:         " + file_f["ID_FullName"] + "\n" +
-            "Name:       " + file_f["Name"] + "\n" +
-            "Folder:     " + str(file_f["Folder"]) + "\n" +
-            "Used:       " + str(file_f["Used"]) + "\n" +
-            "Modified:   " + file_f["Modified"].strftime("%x %X") + "\n" +
-            "Type:       " + file_f["Type"] + "\n" +
-            "Level:      " + str(file_f["Level"]) + "\n" +
-            "Attribute:  " + attribute
-        ))
-
-        break
-
-# Check DeleteFolder
-print("\n\nExpection: Deletion of previously created folder \"42alpha\"")
-state_machine.DeleteFolder(unicode(options.folder + "/42alpha"))
+if __name__ == '__main__':
+    main()

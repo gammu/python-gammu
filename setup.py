68b500d4e5fbcd94661129f97738eef7b2f81dba
Michal Čihař <michal@cihar.com>
2015-04-27 16:12:31 +0200

    List supported python versions
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- setup.py
+++ setup.py
@@ -126,6 +126,11 @@ setup(
         'Operating System :: POSIX',
         'Operating System :: Unix',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: C',
         'Topic :: Communications :: Telephony'
     ],

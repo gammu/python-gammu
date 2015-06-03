eeb7d422ed8d31e6bd2d3d175bd7c5a33cd4ab30
Michal Čihař <michal@cihar.com>
2015-06-03 09:18:46 +0200

    Update link to project web page
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- examples/listfilesystem.py
+++ examples/listfilesystem.py
@@ -4,7 +4,7 @@
 #
 # Copyright © 2003 - 2015 Matthias Blaesing <matthias.blaesing@rwth-aachen.de>
 #
-# This file is part of Gammu <http://wammu.eu/>
+# This file is part of python-gammu <http://wammu.eu/python-gammu/>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- examples/sendlongsms.py
+++ examples/sendlongsms.py
@@ -4,7 +4,7 @@
 #
 # Copyright © 2003 - 2015 Michal Čihař <michal@cihar.com>
 #
-# This file is part of Gammu <http://wammu.eu/>
+# This file is part of python-gammu <http://wammu.eu/python-gammu/>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

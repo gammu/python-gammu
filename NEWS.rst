e572929e32478f852c739cdb517cb54cb093d5d9
Michal Čihař <michal@cihar.com>
2021-11-17 08:42:31 +0100

    setting version to 3.2.4

--- NEWS.rst
+++ NEWS.rst
@@ -1,3 +1,8 @@
+3.2.4
+=====
+
+* Compatibility with Python 3.10.
+
 3.2.3
 =====
 
--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ from distutils.version import StrictVersion
 from setuptools import Extension, setup
 
 # some defines
-VERSION = "3.2.3"
+VERSION = "3.2.4"
 GAMMU_REQUIRED = "1.37.90"
 
 # readme

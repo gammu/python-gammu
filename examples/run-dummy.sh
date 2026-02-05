d28e2c49795f27b916acb6720f3f34c3543afb1d
Michal Čihař <michal@cihar.com>
2026-02-05 15:46:45 +0100

    feat(ci): test more examples
    Also fixes several errors in the files.

--- examples/run-dummy.sh
+++ examples/run-dummy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e
+set -x
 cd $(dirname $0)
 
 TEST_DIR=$(mktemp -d)
@@ -33,9 +35,44 @@ cp -r ../test/data/gammu-dummy/ "$DUMMY"
 mkdir "$DUMMY/fs/test"
 find $TEST_DIR
 
-
-./filesystem_test.py --config "$CONFIG" --folder "test"
-
 export XDG_CONFIG_HOME="$TEST_DIR"
 
+
+./addcontacts.py ME ../test/data/gammu.vcf
+./addfile.py ../test/data/gammu.vcf test
+./async.py
+./backup_convertor.py ../test/data/gammu.vcf "$TEST_DIR/test.backup"
+./batteryinfo.py
+./debugging.py
+./dialvoice.py 1234
+./doc-exceptions.py
+./dummy_phone.py "$CONFIG"
+./filesystem_test.py --config "$CONFIG" --folder "test"
+./getallcalendar.py
+./getallmemory_nonext.py ME
+./getallmemory.py SM
+./getallsms_decode.py
+./getallsms.py
+./getalltodo.py
+# This is an infinite loop
+# ./incoming.py
+./getdiverts.py
+./listfilesystem.py
+./mass_sms.py "Test message" 123 456
 ./network_info.py
+./pdu_decoder.py "079124602009999002AB098106845688F8907080517375809070805183018000"
+#./read_sms_backup.py
+./savesmspercontact.py
+./sendlongsms.py "$CONFIG" 132
+./sendsms.py "$CONFIG" 132
+echo "#123" | ./service_numbers.py
+./setdiverts.py 123
+#./smsbackup.py
+# Needs SMSD
+#./smsd_inject.py
+# Needs SMSD
+#./smsd_state.py
+# This is an infinite loop
+#./sms_replier.py
+./vcs.py
+./worker.py

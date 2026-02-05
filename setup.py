ec288f5c4b9be29f20a800ad021ecf4b09bb8431
Michal Čihař <michal@cihar.com>
2026-02-05 08:39:13 +0100

    chore: extend ruff scope

--- setup.py
+++ setup.py
@@ -19,9 +19,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
-"""
-python-gammu - Phone communication library
-"""
+"""python-gammu - Phone communication library."""
 
 import glob
 import os

e69cc74960a92da052a3521a740c7fdab87dd7b7
Michal Čihař <michal@cihar.com>
2026-02-09 19:07:55 +0100

    Drop compatibility with old Gammu releases

--- gammu/src/convertors/sms.c
+++ gammu/src/convertors/sms.c
@@ -1384,11 +1384,9 @@ char *MultiPartSMSIDToString(EncodeMultiPartSMSID type)
 		case SMS_SiemensFile:
 			s = strdup("SiemensFile");
 			break;
-#if GAMMU_VERSION_NUM >= 13805
 		case SMS_USSD:
 			s = strdup("USSD");
 			break;
-#endif
 	}
 
 	if (s == NULL) {
